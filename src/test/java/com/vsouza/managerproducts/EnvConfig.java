da7ac8302e6c1690047f2098d67f29020b165578
veronica.souza <veronica.souza@icts.com.br>
2024-10-21 20:55:23 -0300

    env de teste

--- src/test/java/com/vsouza/managerproducts/EnvConfig.java
+++ src/test/java/com/vsouza/managerproducts/EnvConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("file:.env")
+@PropertySource("file:.env.test")
 public class EnvConfig {
 }
